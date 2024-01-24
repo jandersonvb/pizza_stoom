3fccbb197aff6d8e1815e4c51e4741caade8dcdb
jandersonvb <jandersonvb.dev@gmail.com>
2024-01-24 15:55:50 -0300

    refactor: add more featured in context

--- src/context/PizzaContext.tsx
+++ src/context/PizzaContext.tsx
@@ -1,22 +1,117 @@
-import { createContext, ReactNode, useContext, useState } from 'react'
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 
-interface PizzaContextData {
-  massa: string
-  recheio: string
-  tamanho: string
+interface PizzaProps {
+  image?: string
+  points?: number
+  dough?: string
+  ingredients?: string
+  size?: string
+  price?: number
 }
 
-export const PizzaContext = createContext({})
+interface PizzaContextData {
+  pizza: PizzaProps
+  points: number
+  massa: string
+  total: number
+  imagePizza: string
+  setMassa: (massa: string) => void
+  setIngredients: (ingredients: string) => void
+  setTamanho: (size: string) => void
+  setImage: (image: string) => void
+  setPrice: (price: number) => void
+  handlePizza: (pizza: PizzaProps) => void
+  sumPoints: (value: number) => void
+  sumTotal: (value: number) => void
+  clearProperties: () => void
+}
+
+export const PizzaContext = createContext({} as PizzaContextData)
 
 export function PizzaContextProvider({ children }: { children: ReactNode }) {
-  const [pizza, setPizza] = useState<PizzaContextData>({
-    massa: '',
-    recheio: '',
-    tamanho: '',
-  })
+  const [pizza, setPizza] = useState<PizzaProps>()
+  const [total, setTotal] = useState<number>(0)
+
+  const [points, setPoints] = useState<number>(0)
+  const [imagePizza, setImagePizza] = useState<string>('')
+
+  useEffect(() => {
+    const points = localStorage.getItem('@pizzaria:points')
+
+    setPoints(Number(points))
+  }, [])
+
+  function sumPoints(value: number) {
+    const sum = value + points
+
+    localStorage.setItem('@pizzaria:points', String(sum))
+
+    setPoints(sum)
+  }
+
+  function sumTotal(number: number) {
+    const newTotal = total + number
+
+    setTotal(newTotal)
+  }
+
+  function setPrice(price: number) {
+    const sum = price + (pizza?.price ?? 0)
+
+    setPizza({ ...pizza, price: sum })
+  }
+
+  function setMassa(dough: string) {
+    setPizza({ ...pizza, dough })
+  }
+
+  function setTamanho(size: string) {
+    setPizza({ ...pizza, size })
+  }
+
+  function setImage(image: string) {
+    setImagePizza(image)
+  }
+
+  function setIngredients(ingredients: string) {
+    setPizza({ ...pizza, ingredients })
+  }
+
+  function handlePizza(item: PizzaProps) {
+    setPizza(item)
+  }
+
+  function clearProperties() {
+    setTotal(0)
+    setImagePizza('')
+    setPizza({} as PizzaProps)
+  }
 
   return (
-    <PizzaContext.Provider value={{ pizza: 'Pizza media' }}>
+    <PizzaContext.Provider
+      value={{
+        pizza: pizza || ({} as PizzaProps),
+        setMassa,
+        setTamanho,
+        setImage,
+        setIngredients,
+        handlePizza,
+        setPrice,
+        sumTotal,
+        total,
+        sumPoints,
+        points,
+        imagePizza,
+        clearProperties,
+        massa: '',
+      }}
+    >
       {children}
     </PizzaContext.Provider>
   )

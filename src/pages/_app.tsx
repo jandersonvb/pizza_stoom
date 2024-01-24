51bab67eebd05ac975dc29aa536dd2c5425dcaab
jandersonvb <jandersonvb.dev@gmail.com>
2024-01-24 15:59:26 -0300

    feat: ensure Header componente be displayed in every application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,13 @@ import type { AppProps } from 'next/app'
 
 import '../styles/global.scss'
 import { PizzaContextProvider } from '@/context/PizzaContext'
+import { Header } from '@/components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PizzaContextProvider>
+      <Header />
+
       <Component {...pageProps} />
     </PizzaContextProvider>
   )
